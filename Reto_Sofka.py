c691b465a149c91b7a8ec1380fe6f4b114a4c54d
reyesda <54370848+reyesda@users.noreply.github.com>
2021-08-06 20:34:23 -0500

    Comentarios

--- Reto_Sofka.py
+++ Reto_Sofka.py
@@ -3,64 +3,77 @@ import random
 
 class jugador:
     def __init__(self, identificador, nombre):
-        self.identificador = identificador
+        self.identificador = identificador  # para asignar su lugar las listas
         self.nombre = nombre
 
 
-class carro(jugador):
+class carro(jugador):  # hereda de jugador
     def __init__(self, identificador, nombre, figura):
         jugador.__init__(self, identificador, nombre)
 
-        self.figura = figura
+        self.figura = figura  # para graficar los carros
         self.posicion = 0  # esta en metros
 
-    def desplazamiento(self, limite):
-        self.posicion += random.randint(1, 6) * 100
+    def desplazamiento(self, limite):  # para mover los carros aleatoriamente por el carril
+        self.posicion += random.randint(1, 6) * 100  # regla de desplazamiento, en metros
+
+        # se limita el desplazamiento máximo al tamaño del carril
         self.posicion = self.posicion if self.posicion <= (limite * 1000) else limite * 1000
 
 
-class carril(carro):
+class carril(carro):  # hereda de carro
     def __init__(self, identificador, nombre, figura, distancia):
         carro.__init__(self, identificador, nombre, figura)
 
-        self.distancia = distancia  # esta en kilometros
-        self.ganador = False
+        self.distancia = distancia  # está en kilometros
+        self.ganador = False  # indica si ya llegó a la meta
 
-    def comprobar(self):
-        if self.posicion == self.distancia * 1000:
+    def comprobar(self):  # verifica si ya llegó a la meta
+        if self.posicion == self.distancia * 1000:  # convierte distancia a metros
             self.ganador = True
 
 
-class pista:
+class pista:  # crea el objeto que almacena todos los carriles
     def __init__(self):
         self.carriles = []
 
+    # llena “carriles” con todos los objetos carriles
     def llenar_carriles(self, identificador, nombre, figura, distancia):
         for i in range(len(identificador)):
             self.carriles.append(carril(identificador[i], nombre[i], figura[i], distancia))
 
+    # llama el método de avanzar para los carros y verificar si ganaron
     def avanzar(self):
         for i in self.carriles:
             i.desplazamiento(i.distancia)
             i.comprobar()
 
 
-class podio:
+class podio:  # clase para guardar la posición de llegada
     def __init__(self):
+        # almacena en orden los identificadores de quienes terminan la carrera
+        # orden_llegada [0] = 2, quiere decir que el conductor con indicador “2” quedó en primer puesto
         self.orden_llegada = []
+        # almacena la posición en la que quedo cada corredor (cada índice según identificador)
+        # resultado [0] = 1, quiere decir que el conductor con indicador “0” quedó en primer puesto
         self.resultado = []
 
+    # llena orden_llegada con el indicador de los corredores en orden de llegada
     def guardar_ganador(self, objeto_pista):
         while len(self.orden_llegada) < len(objeto_pista.carriles):
+            # avanzan todos los carros hasta que todos hayan terminado
             objeto_pista.avanzar()
 
+            # se detecta quien ganó y se guarda en la lista
             for i in objeto_pista.carriles:
                 if i.identificador not in self.orden_llegada:
                     if i.ganador is True:
                         self.orden_llegada.append(i.identificador)
 
+    # cambia el formato de orden_llegada al de resultado
     def ordenar_resultado(self):
         for i in range(len(self.orden_llegada)):
+            # busca el indicador del jugador y lo guarda en su index
             self.resultado.append(self.orden_llegada.index(i) + 1)
 
 
