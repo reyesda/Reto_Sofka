526d199ecf870c3148fdb1cc0b674024b0b931c1
reyesda <54370848+reyesda@users.noreply.github.com>
2021-08-07 12:04:27 -0500

    Actualización documentos y variables

--- Reto_Sofka_main.py
+++ Reto_Sofka_main.py
@@ -1,13 +1,13 @@
 import Reto_Sofka_funciones as Rs_funciones
 
 datos = Rs_funciones.leer_archivo()
-pista1 = Rs_funciones.pista()
-podio1 = Rs_funciones.podio()
+pista = Rs_funciones.pista()
+podio = Rs_funciones.podio()
 
-pista1.llenar_carriles(datos[0], datos[1], datos[2])
+pista.llenar_carriles(datos[0], datos[1], datos[2])
 
-podio1.guardar_ganador(pista1)
-podio1.ordenar_resultado()
+podio.guardar_ganador(pista)
+podio.ordenar_resultado()
 
 Rs_funciones.estadisticas(datos)
-Rs_funciones.actualizar_archivo(podio1)
+Rs_funciones.actualizar_archivo(podio)
